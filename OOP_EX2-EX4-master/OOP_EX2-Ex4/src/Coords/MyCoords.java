1f06c7a18d1f540dd4df98ed14c691b1d66e310e
PAZJR93 <omerpaz93@gmail.com>
2018-11-15 09:14:42 +0200

    adding Add Function in Mycoords Adding Vector3D functin in MyCoords

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Coords/MyCoords.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Coords/MyCoords.java
@@ -12,30 +12,29 @@ public class MyCoords implements coords_converter {
 
 	@Override
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
-		// TODO Auto-generated method stub
-		return null;
+		Point3D meterToGPs = local_vector_in_meter.ConvertToGps();
+		System.out.println(meterToGPs.toString());
+		double ans_x = gps.ix()+meterToGPs.ix();
+		double ans_y = gps.iy()+meterToGPs.iy();
+		double ans_z = gps.iz()+meterToGPs.iz();
+
+		return new Point3D(ans_x,ans_y,ans_z);
 	}
 
 	@Override
 	public double distance3d(Point3D gps0, Point3D gps1) {
 		double diff_p1_x=gps0.x()-gps1.x();
 		double diff_p1_y=gps0.y()-gps1.y();
-
 		double diff_radian_x=(diff_p1_x*Math.PI)/180;
 		double diff_radian_y=(diff_p1_y*Math.PI)/180;
-
 		double long_norm=Math.cos((gps0.x()*Math.PI)/180);
-
 		double to_meter_x=Math.sin(diff_radian_x)*RADUIS_EARTH;
 		double to_meter_y=Math.sin(diff_radian_y)*RADUIS_EARTH*long_norm;
 
 		double result =Math.sqrt((to_meter_x*to_meter_x)+(to_meter_y*to_meter_y));
 
 		return result;
-		
-		
-		
-		
+
 		
 		// TODO Auto-generated method stub
 	
@@ -43,13 +42,21 @@ public class MyCoords implements coords_converter {
 
 	@Override
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Point3D gps0ToMeters = new Point3D(gps0);
+		Point3D gps1ToMeters = new Point3D(gps1);
+		
+		double m_x = gps1ToMeters.ix()-gps0ToMeters.ix();
+		double m_y = gps1ToMeters.ix()-gps0ToMeters.ix();
+		double m_z = gps1ToMeters.ix()-gps0ToMeters.ix();
+
+
+		return new Point3D (m_x,m_y,m_z);
 	}
 
 	@Override
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
@@ -58,5 +65,7 @@ public class MyCoords implements coords_converter {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	
 
 }
--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Coords/coords_converter.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Coords/coords_converter.java
@@ -4,7 +4,7 @@ import Geom.Point3D;
 
 /**
  * This interface represents a basic coordinate system converter, including:
- * 1. The 3D vector between two lat,lon, alt points 
+ * 1. The 3D vector between two lat,lon, alt	 points 
  * 2. Adding a 3D vector in meters to a global point.
  * 3. convert a 3D vector from meters to polar coordinates
  * @author Boaz Ben-Moshe
