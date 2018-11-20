0dcc8ff55ab3d6fad2370243b5a326b95839c37b
Shimon1705 <shimon1705@gmail.com>
2018-11-20 11:13:52 +0200

    found bug csv to kml

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/File_format/Lunch.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/File_format/Lunch.java
@@ -6,9 +6,8 @@ import java.io.IOException;
 public class Lunch {
 
 	public static void main(String[] args) throws IOException {
-		File file = CsvFileHelper.getResource("data/WigleWifi_20171201110209.csv");
+		File file = CsvFileHelper.getResource("OOP_EX2-Ex4/data/WigleWifi_20171201110209.csv");
 		csv2kml.to_KML(csv2kml.readFile(file), "ouput.kml");
 		
 	}
-
 }
