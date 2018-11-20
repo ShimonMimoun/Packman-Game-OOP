9d9cc3d86c7ef1ae8c4079602674b721ffb734d7
Shimon1705 <shimon1705@gmail.com>
2018-11-20 10:49:13 +0200

    Add data

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/File_format/Lunch.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/File_format/Lunch.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Lunch {
 
 	public static void main(String[] args) throws IOException {
-		File file = CsvFileHelper.getResource("src/WigleWifi.csv");
+		File file = CsvFileHelper.getResource("data/WigleWifi_20171201110209.csv");
 		csv2kml.to_KML(csv2kml.readFile(file), "ouput.kml");
 		
 	}
