ae02898c672c5e3bef355cbef6caa3290296fd5c
PAZJR93 <omerpaz93@gmail.com>
2018-11-23 16:04:13 +0200

    fix the elveation in the azimut function in mycoords
    adding junit for mycorrds.

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Coords/MyCoords.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Coords/MyCoords.java
@@ -3,7 +3,8 @@ package Coords;
 import Geom.Point3D;
 
 
-
+//		Point3D gps0=new Point3D(32.103315,35.209039,670);
+//      Point3D gps1=new Point3D(32.106352,35.205225,650);
 
 public class MyCoords implements coords_converter {
 	
@@ -13,23 +14,23 @@ public class MyCoords implements coords_converter {
 
 	@Override
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
-		Point3D covertedCartisian = gps.ConvertToCartesian();
+		Point3D covertedCartisian = ConvertToCartesian(gps);
 		
 		double ans_x = covertedCartisian.x()+local_vector_in_meter.x();
 		double ans_y = covertedCartisian.y()+local_vector_in_meter.y();
 		double ans_z = covertedCartisian.z()+local_vector_in_meter.z();
 
-		Point3D ans = new Point3D(ans_x,ans_y,ans_z);
-		ans.ConvertToGps();
+		Point3D ans = ConvertToGps(new Point3D(ans_x,ans_y,ans_z));
+	
 
-	return ans.ConvertToGps();
+	return ans;
 	}
 
 	@Override
 	public double distance3d(Point3D gps0, Point3D gps1) {
 		
-		Point3D gps0Change = gps0.ConvertToCartesian();
-		Point3D gps1Change = gps1.ConvertToCartesian();
+		Point3D gps0Change = ConvertToCartesian(gps0);
+		Point3D gps1Change = ConvertToCartesian(gps1);
 		return Math.abs(gps0Change.distance3D(gps1Change));
 
 		
@@ -40,8 +41,8 @@ public class MyCoords implements coords_converter {
 	@Override
 	public Point3D vector3D(Point3D gps0, Point3D gps1) {
 		
-		Point3D gps0ToMeters = new Point3D(gps0);
-		Point3D gps1ToMeters = new Point3D(gps1);
+		Point3D gps0ToMeters = ConvertToCartesian(gps0);
+		Point3D gps1ToMeters = ConvertToCartesian(gps1);
 		
 
 		double m_x = gps1ToMeters.x()-gps0ToMeters.x();
@@ -54,23 +55,45 @@ public class MyCoords implements coords_converter {
 
 	@Override
 	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
+
+		double[] ans = new double[3];
+		ans[0] = gps1.north_angle(gps0);
+		ans[1] = Math.toDegrees(Math.acos((gps1.z()-gps0.z())/(distance3d(gps1 , gps0))))-90;
+		ans[2] = distance3d(gps1, gps0);
 		
-		double[] azimuth_elevation_dist = new double[3];
-		azimuth_elevation_dist[0] = gps1.north_angle(gps0);
-		azimuth_elevation_dist[1] = gps1.up_angle(gps0);
-		azimuth_elevation_dist[2] = distance3d(gps1, gps0);
-		
-		return azimuth_elevation_dist;
+		return ans;
 	}
 
 	@Override
 	public boolean isValid_GPS_Point(Point3D p) {
 		
-		
-		
-		return (p.x()<-180 || p.x() >180 ||p.y() <-90 || p.y() >90 || p.z() <-450);
+		return (p.x()<-180 || p.x() >180 ||p.y() <-90 || p.y() >90 || p.z() <-450) ? false : true;
 		
 	
+	}
+	
+	/// covnert functions ///
+	
+	public Point3D ConvertToGps(Point3D p) {		
+
+
+		double r=Math.asin(p.z()/RADUIS_EARTH)*180/Math.PI;
+		double longitude=Math.atan2(p.y(), p.x())*180/Math.PI;
+		double latidude =Math.sqrt(Math.pow(p.x(), 2)+Math.pow(p.y(), 2)+Math.pow(p.z(), 2));
+
+		return new Point3D (r,longitude,latidude);
+
+	}
+
+	public Point3D ConvertToCartesian(Point3D p) {
+
+
+		double Gps_x = Math.cos(p.y()/180*Math.PI) * Math.cos(p.x()/180*Math.PI)*(RADUIS_EARTH+p.z());;
+		double Gps_y =Math.sin(p.y()/180*Math.PI) * Math.cos(p.x()/180*Math.PI)*(RADUIS_EARTH+p.z());
+		double Gps_z = (RADUIS_EARTH+p.z())*Math.sin(p.x()/180*Math.PI);
+
+
+		return new Point3D(Gps_x,Gps_y,Gps_z);
 	}
 	
 	
---
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/Tests/MyCoordsTest.java
@@ -0,0 +1,85 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Coords.MyCoords;
+import Geom.Point3D;
+
+class MyCoordsTest extends MyCoords {
+
+	MyCoords mc;
+	Point3D gps0;
+	Point3D gps1;
+	double expectedValue;
+	double acutalValue;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		gps0=new Point3D(32.103315,35.209039,670);
+		gps1=new Point3D(32.106352,35.205225,650);
+		expectedValue = 0;
+		acutalValue = 0;
+
+		mc = new MyCoords();
+	}
+
+		@Test
+		void testAdd() {
+			
+			Point3D meter = new Point3D(20,30,0);
+			Point3D acutal_added_Point = add(gps0,meter);
+			Point3D expcetedPoint = new Point3D(32.10709532470852,35.20917679211647,6371698.494419785);
+			if(!acutal_added_Point.equals(expcetedPoint)) {
+				fail("You Got wrong with add function");
+
+			}
+				
+		}
+
+	@Test
+	void testDistance3d() {
+		acutalValue = mc.distance3d(gps0, gps1); // check the distance, and the resulte need to be 493.504487890462.
+		if (acutalValue != 493.504487890462) 
+			fail("You Got wrong with claculte the distance");
+	}
+
+	@Test
+	void testVector3D() {
+
+		Point3D p0 = ConvertToCartesian(gps0);
+		Point3D p1 = ConvertToCartesian(gps1);
+		Point3D expctedPoint = new Point3D(p1.x()-p0.x(),p1.y()-p0.y(),p1.z()-p0.z());
+		Point3D acutalPoint  = vector3D(gps0, gps1);
+
+		if(!expctedPoint.equals(acutalPoint))
+			fail("You Got Worng with the verktor3D function");
+
+	}
+
+	@Test
+	void testAzimuth_elevation_dist() {
+		double[] acutal_Azimuth_elevation_dist  = azimuth_elevation_dist(gps0, gps1);
+
+		if(acutal_Azimuth_elevation_dist[0] != 321.47050096915774) 
+			fail("You got worng with the north_angle function in point ");
+
+		if(acutal_Azimuth_elevation_dist[1] != 2.322632366882857) 
+			fail("You got worng with the elevation calcaulte");
+
+		if(acutal_Azimuth_elevation_dist[2] != 493.504487890462) 
+			fail("You got worng with the distance3d at pointe");
+
+	}
+		@Test
+		void testIsValid_GPS_Point() {
+			
+			if(!isValid_GPS_Point(gps0)) {
+				fail("You got worng with the isvalid function");
+
+			}
+		}
+
+}
