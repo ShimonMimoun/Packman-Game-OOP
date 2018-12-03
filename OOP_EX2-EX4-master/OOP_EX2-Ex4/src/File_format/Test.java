6093093a9ad6b81f86588d2aa32cd38abdfbc9de
PAZJR93 <omerpaz93@gmail.com>
2018-12-03 19:32:28 +0200

    adding

---
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/File_format/Test.java
@@ -0,0 +1,23 @@
+package File_format;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+public class Test {
+	
+	private static long UTC;
+	
+	public static void main(String[] args) {
+		
+		Date test= new Date();
+		long time = new Date().getTime();
+
+System.out.println(test.getTimezoneOffset());
+		
+	}
+
+
+
+}
