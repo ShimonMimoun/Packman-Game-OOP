ab41f942aa2b6572445fe1e18da9034bc091b1e0
PAZJR93 <omerpaz93@gmail.com>
2018-12-06 14:38:02 +0200

    dd

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GIS/Furit.java
+++
@@ -1,9 +0,0 @@
-package GIS;
-
-import Geom.Point3D;
-
-public class Furit {
-	
-	Point3D fruitPoint;
-
-}
--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GUI/MyFrameMain.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GUI/MyFrameMain.java
@@ -3,11 +3,12 @@ package GUI;
 import javax.swing.JFrame;
 
 
-public class Main 
+public class MyFrameMain 
 {
 	public static void main(String[] args)
 	{
-		MainWindow window = new MainWindow();
+//		MyFarme window = new MyFarme();
+		MyFameTEST window = new MyFameTEST();
 		window.setVisible(true);
 		window.setSize(window.myImage.getWidth(),window.myImage.getHeight());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
