cea0f354787dbe9531e6edb496ec082c46db69c1
shimon Mimoun <shimon1706@gmail.com>
2018-12-09 18:34:57 +0100

    אקדא

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GUI/MyFarme.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GUI/MyFarme.java
@@ -64,9 +64,9 @@ public class MyFarme extends JFrame implements MouseListener
 	private void initGUI() {
 
 
-		try {	myImage = ImageIO.read(new File("Ariel1.png")); } catch (IOException e) { e.printStackTrace();	}	
-		try {	packimage = ImageIO.read(new File("packman.jpg")); } catch (IOException e) { e.printStackTrace();	}
-		try {	furitimage = ImageIO.read(new File("furit.png")); } catch (IOException e) { e.printStackTrace();	}
+		try {	myImage = ImageIO.read(new File("Images/Ariel1.png")); } catch (IOException e) { e.printStackTrace();	}	
+		try {	packimage = ImageIO.read(new File("Images/packman.jpg")); } catch (IOException e) { e.printStackTrace();	}
+		try {	furitimage = ImageIO.read(new File("Images/furit.png")); } catch (IOException e) { e.printStackTrace();	}
 
 
 
