c3e1f25d0da79046bea42188eac600894f9b9201
Shimon1705 <shimon1705@gmail.com>
2018-12-16 12:16:34 +0200

    fs

--- OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GIS/Game.java
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GIS/Game.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import Algorithm.ShortestPathAlgo;
 import File_format.csv2kml;
 
 import Geom.Point3D;
@@ -130,75 +131,130 @@ public class Game {
 		ArrayList<String> content = new ArrayList<String>();
 		String kmlstart = 
 				"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + 
-						"<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Style id=\"red\">\r\n" + 
+						"<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document>\r\n<name> Points with TimeStamps</name>\r\n <Style id=\"red\">\r\n" + 
 						"<IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href></Icon></IconStyle>\r\n" + 
 						"</Style><Style id=\"Packman\"><IconStyle><Icon><href>http://www.iconhot.com/icon/png/quiet/256/pac-man.png</href></Icon></IconStyle>\r\n" + 
 						"</Style><Style id=\"Fruit\"><IconStyle><Icon><href>http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c316.png</href></Icon></IconStyle></Style>\r\n" + 
 						"\r\n" + 
-						"\r\n" + 
-						"<Folder><name>Wifi Networks</name>\n\n";
+						"    <Style id=\"check-hide-children\">\r\n" + 
+						"      <ListStyle>\r\n" + 
+						"        <listItemType>checkHideChildren</listItemType>\r\n" + 
+						"      </ListStyle>\r\n" + 
+						"    </Style>\r\n" + 
+						"    <styleUrl>#check-hide-children</styleUrl>"+
+						"\r\n";
+
 		content.add(kmlstart);
 		String[] nameData = {"Type","id","Lat","Lon","Speed/Weight"	,"Radius"};
 
-		String kmlend = "</Folder>\n" + 
-				"</Document>\n</kml>";
-		
-		
+		String kmlend = "</Document>\n</kml>";
+
+
+		ShortestPathAlgo algo = new ShortestPathAlgo(this);
+		Path p = algo.algoSinglePackman(this.Packman_arr.get(0));
+		ArrayList<Fruit>my_fruit  = p.TheCurrentPath();
+
 		for (int i = 0; i < Packman_arr.size(); i++) {
 			Packman_arr.get(i).packLocation = theMap.Pixel2GPS(Packman_arr.get(i).getPoint().x(), Packman_arr.get(i).getPoint().y());
 		}
-		for (int i = 0; i < Fruits_arr.size(); i++) {
-			Fruits_arr.get(i).FuritPoint = theMap.Pixel2GPS(Fruits_arr.get(i).getFruitPoint().x(), Fruits_arr.get(i).getFruitPoint().y());
+		for (int i = 0; i < my_fruit.size(); i++) {
+			my_fruit.get(i).FuritPoint = theMap.Pixel2GPS(my_fruit.get(i).getFruitPoint().x(), my_fruit.get(i).getFruitPoint().y());
 
 		}
-		
-		
 
-		for (int i = 0; i < this.Packman_arr.size(); i++) 
+
+		for (int j = 0; j < this.Packman_arr.size(); j++) 
 		{
 
-			String kmlelement ="<Placemark>\n" +
-					"<name><![CDATA[ PACKMAN "+i+"]]></name>\n" +
+			String kmlelement2 ="<Placemark>\n" +
+					"<name><![CDATA[ PACKMAN "+j+"]]></name>\n" +
 					"<description>"+
 					"<![CDATA[B"
 					+nameData[0]+": <b> PACKMAN  </b><br/>"
-					+nameData[1]+": <b> PACKMAN Number"+i+" </b><br/>"
-					+nameData[2]+": <b>"+Packman_arr.get(i).packLocation.x()+" </b><br/>" 
-					+nameData[3]+": <b>"+Packman_arr.get(i).packLocation.y()+" </b><br/>" 
-					+nameData[4]+": <b>"+Packman_arr.get(i).getSpeed()+" </b><br/>" 
-					+nameData[5]+": <b>"+Packman_arr.get(i).getradius()+" </b><br/>" // altito to meter
+					+nameData[1]+": <b> PACKMAN Number"+j+" </b><br/>"
+					+nameData[2]+": <b>"+Packman_arr.get(j).packLocation.x()+" </b><br/>" 
+					+nameData[3]+": <b>"+Packman_arr.get(j).packLocation.y()+" </b><br/>" 
+					+nameData[4]+": <b>"+Packman_arr.get(j).getSpeed()+" </b><br/>" 
+					+nameData[5]+": <b>"+Packman_arr.get(j).getradius()+" </b><br/>" // altito to meter
 
-					+"]]></description>\n" +"<styleUrl>#Packman</styleUrl>"+
+
+
+					+"]]></description>\n" +
+					"<TimeStamp>\r\n" + 
+					"        <when>>2018-11-14T21:05:0"+(j)+"Z</when>\r\n" + 
+					"      </TimeStamp>"+
+					"<styleUrl>#Packman</styleUrl>"+
 					"<Point>\n" +
-					"<coordinates>"+Packman_arr.get(i).packLocation.y()+","+Packman_arr.get(i).packLocation.x()+"</coordinates>" +
+					"<coordinates>"+Packman_arr.get(j).packLocation.y()+","+Packman_arr.get(j).packLocation.x()+"</coordinates>" +
 					"</Point>\n" +
 					"</Placemark>\n";
 
 
-			content.add(kmlelement);
+			content.add(kmlelement2);
 		}
-		for (int i = 0; i < Fruits_arr.size(); i++) 
+
+		
+		
+		
+		
+		
+		
+		for (int i = 0; i < my_fruit.size(); i++) 
 		{
+			int temp=0;
 
 			String kmlelement ="<Placemark>\n" +
-					"<name><![CDATA[ FRUIT  "+i+"]]></name>\n" +
+					"<name><![CDATA[ FRUIT  "+(i)+"]]></name>\n" +
 					"<description>"+
 					"<![CDATA[B"
 					+nameData[0]+": <b> FRUIT  </b><br/>"
 					+nameData[1]+": <b> FRUIT Number"+i+" </b><br/>"
-					+nameData[2]+": <b>"+Fruits_arr.get(i).getFruitPoint().x()+" </b><br/>" 
-					+nameData[3]+": <b>"+Fruits_arr.get(i).getFruitPoint().y()+" </b><br/>" 
-					+nameData[4]+": <b>"+Fruits_arr.get(i).getWeight()+" </b><br/>" 
+					+nameData[2]+": <b>"+my_fruit.get(i).getFruitPoint().x()+" </b><br/>" 
+					+nameData[3]+": <b>"+my_fruit.get(i).getFruitPoint().y()+" </b><br/>" 
+					+nameData[4]+": <b>"+my_fruit.get(i).getWeight()+" </b><br/>" 
 
 
-						+"]]></description>\n" +"<styleUrl>#Fruit</styleUrl>"+
+						+"]]></description>\n" +
+						"<TimeStamp>\r\n" + 
+						"        <when>>2018-11-14T21:05:0"+(i+1)+"Z</when>\r\n" + 
+						"      </TimeStamp>"+
+						"<styleUrl>#Fruit</styleUrl>"+
 						"<Point>\n" +
 						"<coordinates>"+Fruits_arr.get(i).getFruitPoint().y()+","+Fruits_arr.get(i).getFruitPoint().x()+"</coordinates>" +
 						"</Point>\n" +
 						"</Placemark>\n";
 
 
+
 			content.add(kmlelement);
+		
+				String kmlelement2 ="<Placemark>\n" +
+						"<name><![CDATA[ PACKMAN "+temp+"]]></name>\n" +
+						"<description>"+
+						"<![CDATA[B"
+						+nameData[0]+": <b> PACKMAN  </b><br/>"
+						+nameData[1]+": <b> PACKMAN Number"+temp+" </b><br/>"
+						+nameData[2]+": <b>"+Packman_arr.get(temp).packLocation.x()+" </b><br/>" 
+						+nameData[3]+": <b>"+Packman_arr.get(temp).packLocation.y()+" </b><br/>" 
+						+nameData[4]+": <b>"+Packman_arr.get(temp).getSpeed()+" </b><br/>" 
+						+nameData[5]+": <b>"+Packman_arr.get(temp).getradius()+" </b><br/>" // altito to meter
+
+
+
+						+"]]></description>\n" +
+						"<TimeStamp>\r\n" + 
+						"        <when>2018-11-14T21:05:0"+(i+2)+"Z</when>\r\n" + 
+						"      </TimeStamp>"+
+						"<styleUrl>#Packman</styleUrl>"+
+						"<Point>\n" +
+						"<coordinates>"+Fruits_arr.get(i).getFruitPoint().y()+","+Fruits_arr.get(i).getFruitPoint().x()+"</coordinates>" +
+						"</Point>\n" +
+						"</Placemark>\n";
+
+
+				content.add(kmlelement2);
+			
+
 
 		}
 
