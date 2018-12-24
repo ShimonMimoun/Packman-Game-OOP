a9791ff742b20eb5ec61a2959a60542b055f5ce8
Omer Paz <44544244+omerpaz1@users.noreply.github.com>
2018-12-24 19:56:34 +0200

    Add files via upload

---
+++ OOP_EX2-EX4-master/OOP_EX2-Ex4/src/GIS/Player.java
@@ -0,0 +1,18 @@
+package GIS;
+
+import Geom.Point3D;
+
+public class Player {
+	
+	private Point3D p1;
+	private double speed;
+	
+	
+	
+	public Player(Point3D p1, double speed) {
+		
+		this.p1 = p1;
+		this.speed = speed;
+		
+	}
+}
